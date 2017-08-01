0b89154ea1a54a642f75a623865b954366d2b278
ioanachi <ioana_chichi@outlook.com>
2017-08-01 23:04:07 +0300

    fizz buzz

--- intrebare interviu.php
+++ intrebare interviu.php
@@ -1,29 +1,25 @@
 <?php 
 
 
-$i = 1;
+$i = 0;
 while ($i<100) {
+$i++;
 if (($i%3 ==0)&&($i%5 == 0)) {
 	echo $i."-"."fizzbuzz"."<br>";
+	continue;
 }
-elseif ($i%5 == 0){
+if ($i%5 == 0){
 	echo $i."-"."buzz"."<br>";
+	continue;
 }
-elseif ($i%3 == 0){
+if ($i%3 == 0){
 	echo $i."-"."fizz"."</br>";
-}
-else {
-	echo $i."<br>"
-	;
-}
-$i++;
+	continue;
 }
 
-// if ($i%5) {
-	// echo $i."-".buzz"
-// }
-// else echo $i;
-// }
+	echo $i."<br>";
+}
+
 
 
 
