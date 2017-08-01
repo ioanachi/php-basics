2020d2f860f7f04c352460b207905d53fd73d6fb
ioanachi <ioana_chichi@outlook.com>
2017-08-01 22:03:13 +0300

    Cum sa faci un csv

--- excelcsv.php
+++ excelcsv.php
@@ -2,22 +2,37 @@
 
 
 
-$excel = "excel.csv";
-$file = fopen($excel,"r+")or die("unable to open file!");
 
-$read = fread($file, filesize($excel));
+$excel = "excel.csv";
+ $file = fopen($excel,"w")or die("unable to open file!");
+ $result = "NUme, Prenume,Varsta\n";
+$i = 0;
+
+while ($i<= 50) {
+	$varsta = rand(0,50);
+$result =  $result."Turcu,Ciprian".$varsta."\n";
+$i++;
+};
+$wrote = fwrite($file,$result);
+
+// $read = fread($file,filesize($excel));
+// $arr = ["Nume","Prenume","Varsta"];
+
+
+
+
 
 // $completarea = "nume,prenume,varsta";
 
-// $completeaza = fwrite($file, $completarea);
- $objPHPExcel = setActiveSheetIndex(0)
-                            ->setCellValue('A2', "No")
-                            ->setCellValue('B2', "Name")
-                            ->setCellValue('C2', "Email")
-                            ->setCellValue('D2', "Phone")
-                            ->setCellValue('E2', "Address");
+ // $completeaza = fwrite($file, $completarea,50);
+ // $objPHPExcel = setActiveSheetIndex(0)
+                            // ->setCellValue('A2', "No")
+                            // ->setCellValue('B2', "Name")
+                            // ->setCellValue('C2', "Email")
+                            // ->setCellValue('D2', "Phone")
+//                             ->setCellValue('E2', "Address");
 
-echo $objPHPExcel;
+// echo $wrote;
 
 $close = fclose($file);
 ?>
