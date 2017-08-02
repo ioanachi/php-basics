0afc97d2b867d98456a8d0818cdd94480db7b3cb
ioanachi <ioana_chichi@outlook.com>
2017-08-02 21:08:02 +0300

    Negative

---
+++ negative.php
@@ -0,0 +1,32 @@
+<?php 
+
+
+$status = true;
+if (!$status) {
+	echo "Adevarat";
+}
+if (5==5) {
+	echo "5int==5int";
+}
+echo "<br>";
+if (5=="5") {
+	echo "5int==5str";
+}
+echo "<br>";
+
+
+
+
+if (5!=="5") {
+	echo "5int===5str";
+}
+
+
+
+
+
+
+
+
+
+?>
