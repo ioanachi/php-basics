2b2c7ffee6db3e336fbb1bd32f2525008e8fdcb2
ioanachi <ioana_chichi@outlook.com>
2017-08-03 22:03:54 +0300

    rezolvat task 6

--- resolvedTasks/task6.php
+++ resolvedTasks/task6.php
@@ -1,7 +1,29 @@
 <?php
+/*# Arrays Part 3
+
+# TASK:
+
+You have the following array:
+```
+$numbers = [1,2,3,5,10,56,456];
+```
+You need to show the values of the array, separated by a ```-```
+
+**EXAMPLE OUTPUT:**
+``` 1-2-3-5-10-56-456```
+
+
+you will have task6.php available with pre-written code and coments on where to write your code for your task*/
+
+
 //the array you will use
-$numbers = ['a','b','c','asd','mara','x'];
-while($i<= length($numbers)){
+
+$liniuta = "_";
+$numbers = [1,2,3,5,10,56,456];
+foreach($numbers as $key=>$value){
   //WRITE YOUR CODE HERE (as per task requirements
-  $i++;
+echo $value."_";
+
 }
+
+?>
