db7f21bd75c4eff4be945e38555fd18e7b203c37
ioanachi <ioana_chichi@outlook.com>
2017-08-03 22:40:21 +0300

    Arrays 4

--- resolvedTasks/task7.php
+++ resolvedTasks/task7.php
@@ -1,8 +1,28 @@
 <?php
+/*# Arrays Part 4
+
+# TASK:
+
+You have the following array:
+```
+$numbers = ['a','b','c','asd','mara','x'];
+```
+You need to show the values of the array, separated by a ```-```
+
+**EXAMPLE OUTPUT:**
+``` a,b,c,asd,mara,x```
+
+
+you will have task7.php available in the resolvedTasks Directory with pre-written code and coments on where to write your code for your task
+*/
+
 //the array you will use
 $numbers = ['a','b','c','asd','mara','x'];
-while($i<= count($numbers)){
-  //WRITE YOUR CODE HERE (as per task requirements
+echo $numbers[0];
+$i = 0;
+while($i< count($numbers)){
+echo $numbers[$i];
   $i++;
 }
+
 ?>
