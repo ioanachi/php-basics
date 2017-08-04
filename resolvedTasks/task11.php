9f9d04880a7ea5a04c99397b2e91dca1e91e349c
ioanachi <ioana_chichi@outlook.com>
2017-08-04 17:33:54 +0300

    Resolved array reverse

---
+++ resolvedTasks/task11.php
@@ -0,0 +1,29 @@
+<?php
+/*
+# Reverse order of arrays
+
+You have the following array:
+
+```$characters = ['a','b','c','d','x','y','lalala','a','b','c','d','x','y','lalala','a','b','c','d','x','y','lalala'];```
+
+Your mission, if you choose to accept it, is to show on the screen, the contents of the array, reversed, with a delimiter of ```/```. 
+
+Exemple of what your output should be:
+
+```lalala/y/x/d/c/b/a/lalala/y/x/d/c/b/a/lalala/y/x/d/c/b/a```
+
+**NOTE** You must NOT do this manually (you must not change the values manually). Make a dynamic way of reorganizing the code. Hint: Use a loop to cycle between the values of the array and show them.
+*/
+
+$characters = ['a','b','c','d','x','y','lalala','a','b','c','d','x','y','lalala','a','b','c','d','x','y','lalala'];
+
+$reverse = array_reverse($characters);
+$i = count($characters);
+$j = 6;
+foreach ($reverse as $i => $value) {
+	echo $value."/";
+	$i = $i-$j;
+}
+
+
+?>
