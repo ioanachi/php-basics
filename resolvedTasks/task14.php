44f4009dbc64d46c2e7bb45548402b2862e0d7a5
ioanachi <ioana_chichi@outlook.com>
2017-08-05 12:00:11 +0300

    Integrare string in array

--- resolvedTasks/task14.php
+++ resolvedTasks/task14.php
@@ -1,14 +1,44 @@
 <?php
+
+/*append to array
+
+This is a 2 part task:
+
+### Part 1 
+
+You have the following array:
+
+```$arr = ['a','b','c'];```
+
+The array has 3 values. Your task is to add a 4'th value. At the end, The following command will work like this:
+
+```echo $arr[3];``` Will show ```d```
+
+**Hint** Use a function to add (append) the parameter to the array 
+
+# Part 2
+
+You have the following array:
+
+```$arr = ['a','b','c'];```
+
+Add the following array values to it:
+
+```$arr = ['d','e','f'];```
+
+**FINAL**
+Check out task 14.php to see the code you will have to add to solve both parts.
+# */
+
 //the array
 $arr = ['a','b','c'];
+array_push($arr, 'd');
+$var = [$arr, 'd'];
+var_dump($var);
 
-//Your code HERE
-
-
-$echo $arr[3]; //We see the result here
 
 // Just adding some space between the output of parts of this task:
-echo "<br/><br/>";
+/*echo "<br/><br/>";
 
 $arr = ['a','b','c'];
 $arrB = ['d','e','f'];
@@ -17,5 +47,5 @@ $arrB = ['d','e','f'];
 foreach($arrC as $key => $value){
   echo $value."-";
 }
-
+888*/
 ?>
