09f8e04a0cdbcd9a54af78422992746433188ce9
Ciprian Turcu <turcuciprian1@gmail.com>
2017-08-05 07:39:18 +0300

    Create task14.php

---
+++ resolvedTasks/task14.php
@@ -0,0 +1,21 @@
+<?php
+//the array
+$arr = ['a','b','c'];
+
+//Your code HERE
+
+
+$echo $arr[3]; //We see the result here
+
+// Just adding some space between the output of parts of this task:
+echo "<br/><br/>";
+
+$arr = ['a','b','c'];
+$arrB = ['d','e','f'];
+
+// this must output a-b-c-d-e-f
+foreach($arrC as $key => $value){
+  echo $value."-";
+}
+
+?>
