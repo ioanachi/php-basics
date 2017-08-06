76cd25d53bb475261a98de1b4c6ddc9591b25e8b
Ciprian Turcu <turcuciprian1@gmail.com>
2017-08-06 19:26:13 +0300

    Update task21.php

--- tasks/task21.php
+++ tasks/task21.php
@@ -6,4 +6,7 @@ include all the tasks from task 10 to task 20.php into task21.php. In order.
 
 Your solution must be made with 3 lines of code.
 
+PS: write a comment at the very end of the php tag (just before you close it) with the difference between include, require, include_once, require_once
+
+
 
