8091448cbe6df398e6f74a4bfac291742fd0d616
ioanachi <ioana_chichi@outlook.com>
2017-08-06 07:43:57 +0300

    Task 17

---
+++ resolvedTasks/task17.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+#  Generate Dynamic php
+
+You have the following array: 
+```$ArrX = ['Mara','are','mere','multe'];```
+
+Please write dinamically like this:
+
+```
+<p>Mara</p>
+<p>are</p>
+<p>mere</p>
+<p>multe</p>
+```
+
+USe task17.php as a already prewritten file
+
+
+*/
+
+$ArrX = ['Mara','are','mere','multe'];
+
+
+?>
