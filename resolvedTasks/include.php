2b11e2d74ed83d0d162f12b3a4500a7a3c878d7a
ioanachi <ioana_chichi@outlook.com>
2017-08-06 23:03:31 +0300

    Task 21

---
+++ resolvedTasks/include.php
@@ -0,0 +1,24 @@
+<?php
+/*# Include
+
+include all the tasks from task 10 to task 20.php into task21.php. In order.
+
+**CATCH**
+
+Your solution must be made with 3 lines of code.
+
+PS: write a comment at the very end of the php tag (just before you close it) with the difference between include, require, include_once, require_once
+*/
+
+$i=0;
+
+include 'task1'.$i.'.php';
+require 'task11.php';
+include_once 'task12.php';
+// include 'task13.php';
+require_once 'task14.php';
+
+
+
+
+?>
