d39c11b7b4a62f1a7473cd31393875bb1796096e
ioanachi <ioana_chichi@outlook.com>
2017-08-06 07:37:57 +0300

    Task16 foreach

---
+++ resolvedTasks/othertask.php
@@ -0,0 +1,6 @@
+<?php
+$arr = ['white','green','red'];
+echo $arr[1]."<br>".$arr[2]."<br>".$arr[0]."<br>";
+
+
+?>
---
+++ resolvedTasks/task16.php
@@ -0,0 +1,77 @@
+<?php
+/*# Array Push again
+
+You have the following array:
+
+```$arrX = [1,2,3,4,5,6,7,8,9,10];```
+
+You also have the following empty arrays:
+
+```$Pare = [];```
+and
+```$Impare = [];```
+
+programatic, pune numerele pare din ```$arrX``` in ```$Pare``` si numerele impare in ```$impare```
+
+rezultatul trebuie sa arate asa:
+
+```
+$Pare = [2,4,6,8,10];
+$impare = [1,3,5,7,9];
+```
+*/
+
+$arrX = [1,2,3,4,5,6,7,8,9,10];
+$pare = [];
+$impare = [];
+//var_dump($arrX);
+$c = [];
+$i = 0;
+
+/*
+$a = count($arrX)-1;
+while ($i<=$a) {
+if ($arrX[$i]%2 == 0) {
+	array_push($pare,$arrX[$i]);
+}
+else array_push($impare, $arrX[$i]);
+$i++;
+
+}
+
+var_dump($pare);
+var_dump($impare);
+*/
+
+
+
+
+
+
+
+
+foreach ($arrX as $key => $value) {
+
+$d = $value%2;
+
+ if ($value%2 == 0) {
+
+  	 array_push($pare, $value);
+  } 
+
+if ($value%2 !== 0) {
+	# code...
+array_push($impare, $value);
+
+}
+}
+echo "<pre>";
+print_r($pare);
+print_r($impare);
+//var_dump($pare);
+//var_dump($impare);
+
+
+
+
+?>
