c855aac542f2d77dea380beffde9a644bd084c09
ioanachi <ioana_chichi@outlook.com>
2017-08-06 08:31:05 +0300

    added requirements directly in the php file

---
+++ resolvedTasks/task18.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+#  Generate Dynamic php
+
+You have the following array: 
+```$ArrX = ['Mara','are','mere','multe'];```
+
+Please add the values of the array as values for a dropdown box (select input field):
+
+
+USe task17.php as a already prewritten file
+
+### Exemple of a select field with data in it
+```
+<select name="mySelect">
+	option>First option</option>
+	option>Second option</option>
+	option>A value</option>
+</select>
+
+
+*/
+
+$ArrX = ['Mara','are','mere','multe'];
