5303aa5616927a1daa002d887e23460c12ff8ca7
Ciprian Turcu <turcuciprian1@gmail.com>
2017-08-06 18:36:34 +0300

    Update task21.php

--- tasks/task21.php
+++ tasks/task21.php
@@ -1,5 +1,9 @@
 # Include
 
-include all the tasks from task 10 to task 20.php into task21.php. In order
+include all the tasks from task 10 to task 20.php into task21.php. In order.
+
+**CATCH**
+
+Your solution must be made with 3 lines of code.
 
 
