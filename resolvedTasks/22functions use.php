b5b107850d59d93feda32083ddb2f64d6f34377d
ioanachi <ioana_chichi@outlook.com>
2017-08-07 19:16:36 +0300

    22Functions

---
+++ resolvedTasks/22functions use.php
@@ -0,0 +1,39 @@
+<?php
+/*# Functions use
+
+Reading Refference on functions in php https://www.w3schools.com/php/php_functions.asp
+
+You previously completed task22
+
+Create a function that has a variable that will contain the string. The function will return the array. Basically, I'm giving a string to a function with 1 parameter and it will return an array of that string.
+
+**Final result**
+
+If I call the function that you create like this:
+
+```$result = yourFunction('abc');```
+```$result``` will be: ```['a','b','c']```
+
+You must create the function ```stringToArray``` make the functionality to return the array object after you convert it, the result being the above example.
+
+*/
+function result($a){
+  $b = str_split($a);
+  return $b;
+}
+
+
+$x = result('abcdefghijklmnopqrstuvxyz');
+echo "<pre>";
+print_r($x);
+
+
+
+
+
+
+
+
+
+
+ ?>
