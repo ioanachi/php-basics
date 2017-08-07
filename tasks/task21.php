cffc5eb0d8f5c90d2692a8806bb794a0e69a45b3
ioanachi <ioana_chichi@outlook.com>
2017-08-07 17:03:29 +0300

    Incluziuni de fisiere

--- tasks/task21.php
+++ tasks/task21.php
@@ -1,4 +1,5 @@
-# Include
+<?php
+/*# Include
 
 include all the tasks from task 10 to task 20.php into task21.php. In order.
 
@@ -7,6 +8,13 @@ include all the tasks from task 10 to task 20.php into task21.php. In order.
 Your solution must be made with 3 lines of code.
 
 PS: write a comment at the very end of the php tag (just before you close it) with the difference between include, require, include_once, require_once
+*/
 
 
+
+
+
+
+
+?>
 
